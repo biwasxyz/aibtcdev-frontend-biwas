d626b1d57b5e698d5fbe584f38d9d024f6504882
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-03-20 10:45:24 -0700

    fix: remove referrerPolicy from fetch options
    This property wasn't supported on cloudflare in v6 versions of stacks.js, testing if removing it resolves the recent errors.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { getFetchOptions } from "@stacks/network-v6"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const opts = getFetchOptions()
+delete opts.referrerPolicy
