5a833dae036f4004c42f1c8caa599be61c43761a
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-05-19 15:53:32 -0700

    hotfix: expose new dao FACEY

--- src/queries/dao-queries.ts
+++ src/queries/dao-queries.ts
@@ -83,7 +83,7 @@ export const fetchDAOsWithExtension = async (): Promise<DAO[]> => {
             .select("*")
             .order("created_at", { ascending: false })
             .eq("is_broadcasted", true)
-            .in("name", ["MEDIA3", "FACES", "FACE3"]),
+            .in("name", ["MEDIA3", "FACES", "FACE3", "FACEY"]),
         fetchExtensions(),
     ])
 
@@ -107,7 +107,7 @@ export const fetchDAOs = async (): Promise<DAO[]> => {
             .select("*")
             .order("created_at", { ascending: false })
             .eq("is_broadcasted", true)
-            .in("name", ["MEDIA3", "FACES", "FACE3"]),
+            .in("name", ["MEDIA3", "FACES", "FACE3", "FACEY"]),
     ])
 
     if (daosError) throw daosError
