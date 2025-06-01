6fbb0cc7226147f407547a4b0b2276105e3c7b4a
Human <162091348+human058382928@users.noreply.github.com>
2025-05-31 19:08:10 -0700

    update

--- src/app/daos/[name]/mission/page.tsx
+++ src/app/daos/[name]/mission/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+export const runtime = "edge";
+
 export default function MissionPage() {
   // This component is just a placeholder since the actual content
   // is rendered in the layout-client.tsx file when isMission is true
